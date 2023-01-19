f89b643212dbb15ccf680509286152e7bd1ccc92
s314cy <hugo.elguedj@epfl.ch>
2023-01-19 14:31:16 +0100

    better stats display

--- model-checkpoint/src/components/StatsRow.vue
+++ model-checkpoint/src/components/StatsRow.vue
@@ -6,11 +6,11 @@
     <div class="flex flex-col items-center">
       <slot name="icon" />
       <p class="text-4xl text-center space-x-4">
-        <span v-if="props.showTotal">
-          {{ props.total }}
+        <span v-if="props.showTotal" class="flex items-center gap-2">
+          {{ formatNumber(props.total) }} <span class="text-sm">{{ displayUnit(props.total) }}</span>
         </span>
-        <span class="flex items-center gap-2" v-else>
-          {{ props.average }}<span class="text-sm">per client</span>
+        <span v-else class="flex items-center gap-2">
+          {{ formatNumber(props.average) }} <span class="text-sm">{{ displayUnit(props.average) }} per {{ props.per }}</span>
         </span>
       </p>
     </div>
@@ -18,11 +18,47 @@
 </template>
 
 <script setup lang="ts">
-export interface Props {
-  total: number
-  average: number
-  showTotal: boolean
+const props = defineProps({
+  total: {
+    type: Number,
+    required: true
+  },
+  average: {
+    type: Number,
+    default: 0
+  },
+  showTotal: {
+    type: Boolean,
+    default: true
+  },
+  unit: {
+    type: String,
+    required: true
+  },
+  unitPlural: {
+    type: String,
+    required: false
+  },
+  per: {
+    type: String,
+    required: true
+  }
+})
+
+function formatNumber(nbr: number, decimals: number = 1) {
+  let [integer, float] = nbr.toString().split('.')
+  if (integer.length >= 5) {
+    integer = integer.replace(/(\d)(?=(\d{3})+$)/g, '$1 ')
+  }
+  if (float !== undefined) {
+    float = float.slice(0, decimals)
+    return [integer, float].join('.')
+  } else {
+    return integer
+  }
 }
 
-const props = defineProps<Props>()
+function displayUnit (amount: number): string {
+  return amount <= 1 || props.unitPlural === undefined ? props.unit : props.unitPlural
+}
 </script>
